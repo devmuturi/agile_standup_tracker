df5652ea6895db60ff1bae6faa85424e11f116a5
devmuturi <muturinjonge09@gmail.com>
2025-10-21 21:19:01 +0300

    feat: added an application layout

--- .history/app/javascript/controllers/application_20251021204523.js
+++ .history/app/javascript/controllers/application_20251021204523.js
@@ -1,11 +1,7 @@
 import { Application } from "@hotwired/stimulus"
-import Dropdown from "stimulus-dropdown"
-
-
 
 const application = Application.start()
-// Initialize Stimulus application
-application.register("dropdown", Dropdown)
+
 // Configure Stimulus development experience
 application.debug = false
 window.Stimulus   = application
--- .history/config/routes_20251021205652.rb
+++ .history/config/routes_20251021205652.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
   get "activity/mine"
-  get "activity/feed"
   root "activity#feed"
 end
