4323922e44c535ef8bb96eaadf65c65cd9624f31
devmuturi <muturinjonge09@gmail.com>
2025-10-22 20:19:58 +0300

    feat: implement a user management feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,17 @@
 Rails.application.routes.draw do
-  get "users/index"
-  get "users/show"
-  get "users/new"
-  get "users/create"
-  get "users/edit"
-  get "users/update"
   devise_for :users, controllers: {
     registrations: "registrations"
   }
+
+  get "user/me", to: "users#me", as: :my_settings
+  patch "user/update_me", to: "users#update_me", as: :update_my_settings
+  get "user/password", to: "users#password", as: :my_password
+  patch "user/update_password", to: "users#update_password", as: :my_update_password
+
+  scope "account", as: :account do
+    resources :users
+  end
+
   resources :accounts
 
   get "activity/mine"
@@ -15,4 +19,6 @@ Rails.application.routes.draw do
   get "activity/feed"
 
   root "activity#feed"
+
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
